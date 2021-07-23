073f34b0a9973c9b4225ed1613d794d38d68dc61
jokj624 <jokj624@gmail.com>
2021-07-23 16:02:13 +0900

    feat: init 초기 세팅

---
+++ cyworld/src/App.js
@@ -0,0 +1,15 @@
+import { Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>수고했어!</title>
+      </Helmet>
+    </>
+  );
+}
+
+export default App;
